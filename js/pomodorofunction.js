092e380d87cc788adc34c45c16da89a5f4e8f999
Usman Akhter <usmanakhter1@gmail.com>
2018-05-23 06:01:18 -0500

    add play/break switchoff feature

--- js/pomodorofunction.js
+++ js/pomodorofunction.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
 var play = document.querySelector("#play");
+var mode = document.querySelector("#sessionOrBreak");
 var timer = document.querySelector("#theTimer");
 var sessionDuration = 1500;
+var breakDuration = 300;
 
 function secToMin(n) {
 	var min = Math.floor(n/60);
@@ -10,17 +12,36 @@ function secToMin(n) {
 	return min+" mins : "+sec+" secs";
 }
 
-function countdown1() {
-	var countdownInterval = setInterval(countdown2, 1000);
-	function countdown2() {
-		if (sessionDuration>0){
-			sessionDuration--;
-			timer.textContent = secToMin(sessionDuration);
-		}
+function countdownSession() {
+	mode.textContent = "Session Time...   Work!"
+	sessionDuration--;
+	timer.textContent = secToMin(sessionDuration);
+}
+
+function countdownBreak() {
+	mode.textContent = "Break Time...   Take a Break!"
+	breakDuration--;
+	timer.textContent = secToMin(breakDuration);
+}
+
+function together() {
+	if (sessionDuration>0) {
+		countdownSession();
+	}
+	else if (sessionDuration==0 && breakDuration>0) {
+		countdownBreak();
+	}
+	else if (sessionDuration==0 && breakDuration==0) {
+		sessionDuration=1500;
+		breakDuration=300;
+		together();
 	}
 }
 
+function start(){
+	timerInterval = setInterval(together, 1000);
+}
 
-play.addEventListener("click", countdown1)
+play.addEventListener("click", start)
 
 });
