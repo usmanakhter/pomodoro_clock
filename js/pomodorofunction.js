e9b26a01de6d57a23b226a40bc60e320befb2918
Usman Akhter <usmanakhter1@gmail.com>
2018-05-23 03:15:29 -0500

    complete play function for pomodoro clock including stop at 0 sec, no break yet

--- js/pomodorofunction.js
+++ js/pomodorofunction.js
@@ -1,2 +1,26 @@
+$(document).ready(function() {
+
+var play = document.querySelector("#play");
+var timer = document.querySelector("#theTimer");
+var sessionDuration = 1500;
+
+function secToMin(n) {
+	var min = Math.floor(n/60);
+	var sec = n%60;
+	return min+" mins : "+sec+" secs";
+}
+
+function countdown1() {
+	var countdownInterval = setInterval(countdown2, 1000);
+	function countdown2() {
+		if (sessionDuration>0){
+			sessionDuration--;
+			timer.textContent = secToMin(sessionDuration);
+		}
+	}
+}
 
 
+play.addEventListener("click", countdown1)
+
+});
