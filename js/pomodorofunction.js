42334fe2878c579c8c1633452c66a0f714fe33c8
Usman Akhter <usmanakhter1@gmail.com>
2018-05-24 01:02:49 -0500

    finish remaining functions: pause, refresh, stop, session duration editing; multiple play firing bug still needs to be fixed

--- js/pomodorofunction.js
+++ js/pomodorofunction.js
@@ -1,10 +1,54 @@
 $(document).ready(function() {
 
+var sessionShow = document.querySelector("#sessionShow");
+var sessionDown = document.querySelector("#sessionDown");
+var sessionUp = document.querySelector("#sessionUp");
+var breakShow = document.querySelector("#breakShow");
+var breakDown = document.querySelector("#breakDown");
+var breakUp = document.querySelector("#breakUp");
+
+var sessionDuration = 1500;
+var userEditedSessionDuration = sessionDuration;
+var breakDuration = 300;
+var userEditedBreakDuration = breakDuration;
+
+
 var play = document.querySelector("#play");
+var refresh = document.querySelector("#refresh");
+var pause = document.querySelector("#pause");
+var stop = document.querySelector("#stop");
 var mode = document.querySelector("#sessionOrBreak");
 var timer = document.querySelector("#theTimer");
-var sessionDuration = 1500;
-var breakDuration = 300;
+
+
+function timerSettings(){
+	if (this.id=='sessionDown'){
+		if(sessionDuration>0){
+		userEditedSessionDuration=userEditedSessionDuration-60;
+		sessionDuration=userEditedSessionDuration;
+		sessionShow.textContent = secToMin(userEditedSessionDuration);
+		timer.textContent = secToMin(userEditedSessionDuration);
+		}
+	}
+	else if (this.id=='sessionUp'){
+		userEditedSessionDuration=userEditedSessionDuration+60;
+		sessionDuration=userEditedSessionDuration;
+		sessionShow.textContent = secToMin(userEditedSessionDuration);
+		timer.textContent = secToMin(userEditedSessionDuration);
+	}
+	else if (this.id=='breakDown'){
+		if(breakDuration>0){
+		userEditedBreakDuration= userEditedBreakDuration-60;
+		breakDuration= userEditedBreakDuration;
+		breakShow.textContent = secToMin(userEditedBreakDuration);
+		}	
+	}
+	else if (this.id=='breakUp'){
+		userEditedBreakDuration=userEditedBreakDuration+60;
+		breakDuration = userEditedBreakDuration;
+		breakShow.textContent = secToMin(userEditedBreakDuration);
+	}
+}
 
 function secToMin(n) {
 	var min = Math.floor(n/60);
@@ -32,16 +76,53 @@ function together() {
 		countdownBreak();
 	}
 	else if (sessionDuration==0 && breakDuration==0) {
-		sessionDuration=1500;
-		breakDuration=300;
+		sessionDuration=userEditedSessionDuration;
+		breakDuration=userEditedBreakDuration;
 		together();
 	}
 }
 
-function start(){
+var timerInterval;
+function player(){
 	timerInterval = setInterval(together, 1000);
 }
 
-play.addEventListener("click", start)
+function refresher(){
+	clearInterval(timerInterval);
+	sessionDuration=1500;
+	userEditedSessionDuration=sessionDuration
+	sessionShow.textContent= secToMin(userEditedSessionDuration);
+	breakDuration=300;
+	userEditedBreakDuration=breakDuration;
+	breakShow.textContent= secToMin(userEditedBreakDuration);
+	timer.textContent = secToMin(sessionDuration);
+	mode.textContent = "Refreshed"
+}
+
+function pauser(){
+	clearInterval(timerInterval);
+	mode.textContent = "Paused"
+}
+
+function stopper(){
+	clearInterval(timerInterval);
+	sessionDuration=userEditedSessionDuration;
+	breakDuration=userEditedBreakDuration;
+	timer.textContent = secToMin(sessionDuration);
+	mode.textContent = "Stopped"
+}
+
+sessionDown.addEventListener("click", timerSettings);
+sessionUp.addEventListener("click", timerSettings);
+breakDown.addEventListener("click", timerSettings);
+breakUp.addEventListener("click", timerSettings);
+
+play.addEventListener("click", player);
+refresh.addEventListener("click", refresher);
+pause.addEventListener("click", pauser);
+stop.addEventListener("click", stopper);
+
+
+
 
 });
